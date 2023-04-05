44f93c9ddf9162e354af94bce56d94861751e7b1
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2023-04-05 14:14:38 +1000

    add api app, configure urls

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,8 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text)
+get_response = requests.get(endpoint, json={"query": "Hello world!"})
+# print(get_response.text)
 print(get_response.json())
-print(get_response.statu_code)
+# print(get_response.statu_code)
